e66012aacb44f77971eab6d0b107dc96241a0aaf
zfusername <farrukh_ziyaev@epam.com>
2023-07-06 16:56:18 +0500

    Update apps

--- src/main/java/school/mjc/stage0/module3/task1/UpdatedApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/UpdatedApp.java
@@ -2,6 +2,7 @@ package school.mjc.stage0.module3.task1;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        System.out.println("fixMe");
+        System.out.println("I'm updated\n" +
+                "can be compiled. And can write to console");
     }
 }
